96c52b86fce993d84da4584b87ccbe4cda70d7bf
Mohammadreza <mrzfvl3908@gmail.com>
2026-05-06 15:36:16 +0330

    edit model in blog_app and not migrations

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    this is  a class to define posts for blog app
+    """
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=200)
+    image = models.ImageField(blank=True, null=True)
+    content = models.TextField()
+    status = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
