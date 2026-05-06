69d9a78509eb1f024abcc020303704076fbbee03
Mohammadreza <mrzfvl3908@gmail.com>
2026-05-06 16:31:49 +0330

    edit model accounts_app and not migration

--- accounts_app/models.py
+++ accounts_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    # is_verified = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=100)
+    REQUIRED_FIELDS = []
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
