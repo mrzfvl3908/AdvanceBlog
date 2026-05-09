3d3b7d8304034780b472670d3d32a46bc1337a15
Mohammadreza <mrzfvl3908@gmail.com>
2026-05-09 12:56:48 +0330

    edit and add code in admin.py for accounts_app

--- accounts_app/admin.py
+++ accounts_app/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts_app.models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'is_active', 'is_superuser',)
+    list_filter = ('email', 'is_active', 'is_superuser',)
+    search_fields = ('email',)
+    ordering = ('email',)
+    fieldsets = (
+        ('Authentication', {
+            "fields": ('email', 'password'),
+        }),
+        ('permissions', {
+            "fields": ('is_active', 'is_staff','is_superuser'),
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2','is_staff', 'is_active','is_superuser',)
+        }),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
