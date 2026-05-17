8a32c995604fec48a406206419e770c22bf319f8
Mohammadreza <mrzfvl3908@gmail.com>
2026-05-17 11:34:18 +0330

    add listView in blog_app and edit urls.py in blog_app

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts_list', views.IndexView.as_view(), name='index'),
+    # path('', views.IndexView.as_view(), name='index'),
+    path('post/', views.PostListView.as_view(), name='post_list'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.views.generic.base import TemplateView
 from blog_app.models import Post
 
 
-class IndexView(TemplateView):
-    template_name = 'blog_app/posts_list.html'
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['posts'] = Post.objects.all()
-        return context
+# class IndexView(TemplateView):
+#     template_name = 'blog_app/post_list.html'
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['posts'] = Post.objects.all()
+#         return context
+
+
+class PostListView(ListView):
+    # model = Post
+    queryset = Post.objects.filter(status=True).order_by('-created_date')
+    context_object_name = 'posts'
+    paginate_by = 1
+
+    # def get_queryset(self):
+    #     posts = Post.objects.filter(status=True)
+    #     return posts
